c73ec0bc95fc712c68f9587ff09ddaf800e3a71e
zhanghao <15928737095@163.com>
2018-10-17 21:07:43 +0800

    json 序列化与反序列化操作

---
+++ isMe/isMe_Four/高级语法正则表达式与json/j1-json开始.py
@@ -0,0 +1,20 @@
+"""
+1 什么是json javaScript Object Notation 的一个简写
+          javaScript 对象标记
+2 json的本质就是一种轻量级的数据交换格式
+3 json是什么 json对象是什么 json字符串又是什么
+4 json就是一种数据格式 --- 其中字符串就是json的一个表现形式
+  符合json格式的字符串就叫做json字符串 因为json的本质就是一种数据格式 其中字符串就是json的表现形式
+
+5 标准的字符串json格式
+  {"name":"beijing"}  这个也就是字典形式个字符串
+
+
+6 json的优势
+  1 易于阅读
+  2 易于解析         夸语言交换数据
+  3 网络传输的效率高
+
+7 
+
+"""
