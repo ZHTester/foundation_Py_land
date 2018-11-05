07ba85a27e8aeddcfbe9eabb0496b615cf41ba60
zhanghao <15928737095@163.com>
2018-11-06 07:02:40 +0800

    高级函数编程 lambad  三元表达式 map

---
+++ isMe/isMe_Six/高级语法_函数式编程/h3-map.py
@@ -0,0 +1,20 @@
+"""
+map(class类)的时候 函数式编程
+1 map类适用于的场景 -
+
+map 也就是会全部去取值list中的值，然后每次执行square函数
+map 就是映射
+map 也就是相当于执行了一次for循环
+"""
+
+list_x = [1, 2, 3, 4, 5, 6, 7]
+
+
+def square(x):
+    return x * x
+
+
+r = map(square, list_x)
+
+print(list(r))
+
