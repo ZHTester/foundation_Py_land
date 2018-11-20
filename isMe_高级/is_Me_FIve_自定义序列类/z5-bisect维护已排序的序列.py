55b572f1d01639f5531cc6e5381b9d1c408a7058
zhanghao <15928737095@163.com>
2018-11-21 06:11:03 +0800

    深入理解python中的面向对象模式 - 完结  第一轮

---
+++ isMe_高级/is_Me_FIve_自定义序列类/z5-bisect维护已排序的序列.py
@@ -0,0 +1,10 @@
+"""
+说明: bisect 其实就是用来处理已排序的序列，其中list只是序列中的一种，用来维持已排序的序列，且这个序列是升序
+
+1 bisect 其实就是使用二分查找来维持我们的可排序的序列 插入 查找应该放在的那个位置
+  insort = insort_right   # backward compatibility
+
+
+"""
+import bisect
+
