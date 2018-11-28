52456e14ea4099cbdea5b68ae97b2b04dfda6704
Landing <15928737095@163.com>
2018-11-28 15:13:49 +0800

    new&&init区别

--- isMe_高级/is_Me_Eight_元类编程/y3-属性描述符和属性查找过程.py
+++ isMe_高级/is_Me_Eight_元类编程/y3-属性描述符和属性查找过程.py
@@ -2,13 +2,10 @@
 """
 属性描述符和属性查找过程
 
-1 如果说User是一张表，那么字段我们有时候需要去判断他的字符串类型 比如说email 还是 name
-2 数据描述符与非数据属性描述符，对我们的属性的查找顺序是不一样的
-3 
+如果说User是一张表，那么字段我们有时候需要去判断他的字符串类型
 
 """
 from datetime import date, datetime
-import numbers
 
 """
 这个类如何变为属性描述符
@@ -20,31 +17,17 @@ import numbers
 
 """
 
-# 这个类就是属性描述符的对象 所以说age就是一个属性描述符号的对象
-# 这样的方式我们就控制到了，在属性设置的时候的一个参数检查
+
 class IntFeild:
-    # 数据属性描述符
     def __get__(self, instance, owner):
-        return self.value
+        pass
 
     def __set__(self, instance, value):
-        if not isinstance(value,  numbers.Integral):
-            raise ValueError("int value need ")
-
-        if value<0:
-            raise ValueError("你的值需要大于0")
-        # 将值保存起来 self就是指代当前的类对象<------>IntFeild
-        self.value = value
         pass
 
     def __delete__(self, instance):
         pass
 
-
-class NonDataIntField:
-    # 非数据属性描述符
-    def __get__(self, instance, owner):
-        pass
 
 class User:
     """
@@ -61,52 +44,34 @@ class User:
     age = IntFeild()
 
 
-# class User:
-#     def __init__(self, name, email, birthday):
-#         self.name = name
-#         self.birthday = birthday
-#         self.email = email
-#         self._age = 0
-#
-#     # def get_age(self):
-#     #   return datetime.now().year - self.birthday.year  # 返回当前时间点
-#
-#     """
-#     1 这样的模式就是将取函数的模式变为取属性的模式了
-#
-#     """
-#
-#     @property
-#     def age(self):
-#         return datetime.now().year - self.birthday.year  # 返回当前时间点
-#
-#     @age.setter
-#     def age(self, value):
-#         # 检查是不是字符串类型其中age是int类型
-#         self._age = value
+class User:
+    def __init__(self, name, email, birthday):
+        self.name = name
+        self.birthday = birthday
+        self.email = email
+        self._age = 0
+
+    # def get_age(self):
+    #     return datetime.now().year - self.birthday.year  # 返回当前时间点
+
+    """
+    1 这样的模式就是将取函数的模式变为取属性的模式了
+    
+    """
+
+    @property
+    def age(self):
+        return datetime.now().year - self.birthday.year  # 返回当前时间点
+
+    @age.setter
+    def age(self, value):
+        # 检查是不是字符串类型
+        self._age = value
 
 
 if __name__ == "__main__":
-    user = User()
-    """
-    这里进行赋值的时候实际上是会调用，属性描述符中的set方法
-    def __set__(self, instance, value):
-    这个时候instace 就是User value就是我们设定的值30 
-    
-    """
-    user.age = 30
-    """
-    ValueError: int value need 
-    如果这里的值不是int类型那么自然就会报错了
-    ----
-    raise ValueError("int value need ")
-    ValueError: int value need 
-    """
-    # user.age = 'abc'
-    print(user.age)
-    pass
-    # user = User("landing", date(1988, 3, 1))
-    # user.age = 44
-    # # 这样也就是取属性的模式来进行调用函数
-    # print(user._age)  # 设置值
-    # print(user.age)  # 动态属性的值
+    user = User("landing", date(1988, 3, 1))
+    user.age = 44
+    # 这样也就是取属性的模式来进行调用函数
+    print(user._age)  # 设置值
+    print(user.age)  # 动态属性的值
---
+++ isMe_高级/is_Me_Eight_元类编程/y4-new&&init的区别.py
@@ -0,0 +1,50 @@
+# coding=utf-8
+"""
+1 __new__ && __init__的区别 ----高级python开发会经常问道这个问题
+2 def __new__(cls, *args, **kwargs): 这个是在python2.2以后才有的版本，也就是新试类中才有，老版本的python是没有的。
+  在这里面，我们可以再生成User这个对象之前加逻辑，其实这样的一个类就是用来描述类实例化的一个过程,这里面传递的是类cls
+3 def __init__(self): 这里面传递的就是实例
+4 这样的赋值方式 user = User(name='landing') 就会进入到__new__这个类初始化函数当中 kwargs-当中
+5 这样的复制方式 user = User('landing') 就会进入到__new__当中 args当中
+
+"""
+
+
+class User:
+    def __new__(cls, *args, **kwargs):
+        print('in new ')
+        return super().__init__(cls)
+
+    def __init__(self, name):
+        print('in init')
+        self.name = name
+
+
+# new是用来控制对象的生成过程，在对象生成之前
+# init就是用来完善对象的属性
+# 如果new方法不返回对象，则不会调用init函数
+
+
+if __name__ == "__main__":
+    user = User(name='landing')
+    # user = User('landing')
+    pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
