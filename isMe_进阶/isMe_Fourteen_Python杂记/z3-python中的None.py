fd680b11070dbf0d142155521cb7d5722de0c12e
Landing <15928737095@163.com>
2018-12-14 10:13:27 +0800

    格式调节

--- isMe_进阶/isMe_Fourteen_Python杂记/z3-python中的None.py
+++ isMe_进阶/isMe_Fourteen_Python杂记/z3-python中的None.py
@@ -8,6 +8,7 @@ Python 中的None
 """
 print(type(None))
 a = []
+
 # 这就是进行一个判空操作 但是尽量不要使用 a is none (因为None本身就是一个对象 而is就是做对象比较的)
 # 这就是判空的操作
 
@@ -21,9 +22,11 @@ if not a:
 else:
     print("这不是一个空....")
 
+
 # None 与 False本质上的区别  ----> [] 空序列 -----> '' 空字符串
 # None是不存在的概念  而False本质上是真假的判断
 # 这两个本质上得到的结果是一样的但是本质上是不一样的
+
 
 if None:
     pass
