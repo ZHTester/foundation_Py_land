da4085889443d77e62d261e05d39140db5360bfa
Landing <15928737095@163.com>
2018-12-19 17:55:07 +0800

    格式调节

--- isMe_高级/is_Me_Eight_元类编程/y1-property动态属性.py
+++ isMe_高级/is_Me_Eight_元类编程/y1-property动态属性.py
@@ -12,24 +12,32 @@ class User:
     def __init__(self, name, birthday):
         self.name = name
         self.birthday = birthday
-        self._age = 0
+        # self._age = 0
     # def get_age(self):
     #     return datetime.now().year - self.birthday.year  # 返回当前时间点
 
     """
     1 这样的模式就是将取函数的模式变为取属性的模式了
+    2 这样也就是将age变为属性描述符了，这样我么在调用age函数的时候，就和调用属性一样的调用了。 
+    3 动态属性获取自己的方法逻辑
+    
     """
     @property
     def age(self):
         return datetime.now().year - self.birthday.year  # 返回当前时间点
 
+    """
+    1 这里添加了下划线，也就是属性描述符的性能或者方法名称来访问的一种形式
+    2 动态属性设置 变量的值，通过属性方法来调度
+    
+    """
     @age.setter
-    def age(self,value):
+    def age(self, value):
         self._age = value
 
 
 if __name__ == "__main__":
-    user = User("landing", date(1988, 3, 1))
+    user = User("landing", date(year=1987, month=3, day=1))
     user.age = 44
     # 这样也就是取属性的模式来进行调用函数
     print(user._age)  # 设置值
