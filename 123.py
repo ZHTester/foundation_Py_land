db3a7c2f17dbcf0480724075f2fc937b0ff5a97e
Landing <15928737095@163.com>
2018-12-28 15:30:36 +0800

    整理

--- 123.py
+++
@@ -1,9 +0,0 @@
-import json
-
-a = '[{"name": "landing", "age": 18},{"name": "landing", "age": 18}]'
-
-s = json.loads(a)
-print(type(s))
-
-s1 = json.dumps(a)
-print(type(s1))
---
+++ python函数/__init__.py
@@ -0,0 +1,3 @@
+# coding=utf-8
+__author__ = 'landing'
+__data__ = '2018/12/28  15:30'
