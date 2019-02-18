c2e3a19030c8a8c6e1f144eb084fc47b7656f6ff
Landing <15928737095@163.com>
2019-02-18 11:16:11 +0800

    yield

--- isMe_高级/isMe12_Twelve_协程和异步io/x10-生成器实现协程.py
+++ isMe_高级/isMe12_Twelve_协程和异步io/x10-生成器实现协程.py
@@ -20,12 +20,15 @@ import inspect
 
 # 用同步的方式编写异步的代码,在适当的时候暂停函数，在适当的时候启动函数
 def download_url(url):
+    pass
+
 
 def download_html():
-    html = yield from
+    html = yield from download_url
+
 
 if __name__ == "__main__":
-    gen = gen_func()
+    gen = download_html()
     # 这样也就是为了实现了协程提供了基础的保证
     # 获取生成器的状态
     print(inspect.getgeneratorstate(gen))  # GEN_CREATED
