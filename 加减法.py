5085e4b8cf96d63dcd2bc2069fa7f49c7340483f
Landing <15928737095@163.com>
2019-02-19 09:18:46 +0800

    yield

---
+++ 加减法.py
@@ -0,0 +1,48 @@
+# coding=utf-8
+__author__ = 'landing'
+__data__ = '2019/2/18  18:45'
+import threading
+from threading import Lock
+
+a = 0
+
+lock = Lock()
+
+
+class Adds(threading.Thread):
+    def __init__(self, name):
+        super().__init__(name=name)
+
+    def run(self):
+        global a
+        global lock
+        for i in range(1000000):
+            lock.acquire()
+            a += 1
+            lock.release()
+
+
+class Desc(threading.Thread):
+    def __init__(self, name):
+        super().__init__(name=name)
+
+    def run(self):
+        global a
+        global lock
+        for i in range(1000000):
+            lock.acquire()
+            a -= 1
+            lock.release()
+
+
+if __name__ == "__main__":
+    Thr_a = Adds("我是加法")
+    Thr_b = Desc("我是减法")
+
+    Thr_a.start()
+    Thr_b.start()
+
+    Thr_a.join()
+    Thr_b.join()
+
+    print(a)
--- 登录登出压测脚本.py
+++ 登录登出压测脚本.py
@@ -2,42 +2,62 @@
 __author__ = 'landing'
 __data__ = '2019/2/8  16:39'
 
+"""
+10 人的登录登出  
+
+1 首先需要2个线程 2个方法 1-login 2-loginout
+2 线程之间的通讯   - lock Queue(消息队列) 
+3 threading 线程库 
+                           target - 线程要运行的什么东西     args 传入参数 tuple 传入一个参数 args=(5,)
+4 login = threading.Thread(target="",args=(5))  --- 线程的方式 
+5 login.start()
+6 login.join()
+
+import threading
+from threading import Lock
+from queue import Queue
+import requests
+
+"""
 import requests
 import threading
 from threading import Lock
 from queue import Queue
 
 lock = Lock()
-que = Queue()
+que = Queue()  # 消息队列 对象
 
 
+# 登录接口
 def get_login(queue, url, data):
     lock.acquire()  # 加一把锁
-    res = requests.post(url=url, data=data).json()
+    res = requests.post(url=url, data=data).json() # post请求 200
     oid = res["data"]["oid"]
     name = res["data"]["username"]
-    queue.put(oid)
-    queue.put(name)
+    queue.put(oid)  # 往消息队列放入数据
+    queue.put(name)  # 往消息队列放入数据
     lock.release()
-    print("登录用户", name, res)
+    print("登录用户------", name, res)
 
 
 def get_loginout(queue):
     lock.acquire()
-    oid = queue.get()
-    name = queue.get()
+    oid = queue.get()  # 取数据了
+    name = queue.get()  # 取数据了
     data_out = {"username": name, "oid": oid}
     url_out = "http://1029a.s1119.com/m/php//action.php?action=logout"
     res = requests.post(url=url_out, data=data_out).json()
+
     lock.release()
-    print("登出用户", name, res)
+    print("登出用户------", name, res)
 
 
-for i in range(2, 20):
-    q = Queue(maxsize=10)
-    name = 'dstest000' + str(i)
+for i in range(2, 10):
+    q = Queue(maxsize=10)  # 消息队列
+    name = 'dstest000' + str(i)  # 造数据 dest0001 int txt excle mysql
     data = {"username": name, "password": "aeuio888", }
     url_login = "http://1029a.s1119.com/m/php/action.php?action=login"
+
     thread_login = threading.Thread(target=get_login, args=(q, url_login, data))
     thread_loginout = threading.Thread(target=get_loginout, args=(q,))
 
